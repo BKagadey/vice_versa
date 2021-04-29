acf3347d54b813e066c1107e5bd64ebf475abd24
Bohdan Kahadei <kagadey2@gmail.com>
2021-04-29 23:36:49 +0300

    Worlds count

--- config/views.py
+++ config/views.py
@@ -9,4 +9,5 @@ def home(request):
 def reverse(request):
     user_text = request.GET['usertext']
     reversed_text = user_text[::-1]
-    return render(request, 'reverse.html', {'user_text': user_text, 'reversed_text': reversed_text})
+    string_count = len(user_text.split())
+    return render(request, 'reverse.html', {'user_text': user_text, 'reversed_text': reversed_text, 'string_count': string_count})
