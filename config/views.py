2ac9c5e5feed2a2c61130a04b171264bd1c3973e
Bohdan Kahadei <kagadey2@gmail.com>
2021-04-29 23:31:02 +0300

    Add reverse

--- config/views.py
+++ config/views.py
@@ -3,8 +3,10 @@ from django.shortcuts import render
 
 
 
-def about(request):
-    return HttpResponse('This is about!')
-
 def home(request):
     return render(request, 'home.html', {'hello': 'Hello!'})
+
+def reverse(request):
+    user_text = request.GET['usertext']
+    reversed_text = user_text[::-1]
+    return render(request, 'reverse.html', {'user_text': user_text, 'reversed_text': reversed_text})
