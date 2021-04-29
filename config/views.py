e9a2e44e325e6d6f70339d8714fbaff24af01ff5
Bohdan Kahadei <kagadey2@gmail.com>
2021-04-29 23:02:53 +0300

    First commit

---
+++ config/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+
+def about(request):
+    return HttpResponse('This is about!')
+
+def home(request):
+    return render(request, 'home.html', {'hello': 'Hello!'})
